28d7619d5551af71db2768b1173bb5c0acd15f8f
Mohamed Ashraf <mohammed.ashraaf.15@gmail.com>
2022-02-08 18:22:40 +0200

    revert the updates for the chathead

--- public/assets/js/chatbot.js
+++ public/assets/js/chatbot.js
@@ -1,46 +1,50 @@
-// Make chatbot open by default.
-var isChatOpen = false;
-var isChatBotActive = false;
-/* Open and closes the chatbot. */
-$('#chat-button button').click(function () {
-  if (isChatOpen) {
-    // Close chatbot.
-    $('#chatbox').css('display', 'none');
-    $('#chatbot').css('height', '32px');    
-    // Update boolean to reflect that chatbot is now closed.
-    isChatOpen = false;
-  }
-  else {
-    // Open chatbot.
-    $('#chatbox').css('display', 'flex');
-    increaseChatHeight();
-    // Update boolean to reflect that chatbot is now open.
-    isChatOpen = true;
-    if(!isChatBotActive)
-    {
-      isChatBotActive = true;
-      chatRequested();
-    }
-  }
-});
+var chat= 0;
+$('.chat-input input').keyup(function(e) {
+  if ($(this).val() == '')
+    $(this).removeAttr('good');
+  else
+    $(this).attr('good', '');
 
-/* Sets the chatbot height when the window is resized. */
-$(window).resize(function () {
-  if (isChatOpen) {
+});
+$('#chat-button button').click(function(){
+  var height = $(".chatbox").css( "height" ).split("px");
+  if(chat % 2 == 0)
+  {
+    $('#chatbox').css('display', 'none');
+    $('#chatbot').css('height', '32px');
+  }
+  else
+  {
+    $('#chatbox').css('display', 'flex');
+  changeChatSize();
+
+  }
+  chat++;
+    
+});
+$(window).resize(function()
+{
+  if(chat % 2)
+  {
     return;
   }
-  increaseChatHeight();
+  changeChatSize();
 });
 
-/* Displays the chatbot by setting its height for different screen sizes. */
-function increaseChatHeight() {
-  if (window.matchMedia('(max-width: 767px)').matches) {
-    $('#chatbot').css('height', "22em");
-  }
-  else if (window.matchMedia('(max-width: 992px)').matches) {
-    $('#chatbot').css('height', "27em");
-  }
-  else {
-    $('#chatbot').css('height', "37em");
-  }
+function changeChatSize()
+{
+  if (window.matchMedia('(max-width: 767px)').matches)
+    {
+      $('#chatbot').css('height', "22em");
+    }
+    else if (window.matchMedia('(max-width: 992px)').matches)
+    {
+      $('#chatbot').css('height', "27em");
+    }
+    else
+    {
+      $('#chatbot').css('height', "37em");
+
+    }
 }
+
