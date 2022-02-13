50b58bb8aef8af61329a79bd04fa78949567d7cd
Mohamed Ashraf <mohammed.ashraaf.15@gmail.com>
2022-02-13 02:48:05 +0200

    Add icon for the chat

--- public/assets/js/chatbot.js
+++ public/assets/js/chatbot.js
@@ -1,12 +1,14 @@
 // Make chatbot closed by default.
 var isChatOpen = false;
 var isBotCreated = false;
-$('#chat-button button').click(function(){
+$('#chat-button').click(function(){
   if(isChatOpen)
   {
     // Close chatbot.
     $('#webchat').css('display', 'none');
     $('#chatbot').css('height', '32px'); 
+    $("#chat-button").css("bottom", "1.5em");
+
     // Update boolean to reflect that chatbot is now closed.
     isChatOpen = false;  
   }
@@ -40,16 +42,22 @@ function increaseChatHeight() {
   if(!isChatOpen)
   {
     $('#chatbot').css('height', '32px'); 
-    console.log("hello")
+    $("#chat-button").css("bottom", "1.5em");
     return;
   }
   if (window.matchMedia('(max-width: 767px)').matches) {
     $('#chatbot').css('height', "22em");
+    $("#chat-button").css("bottom", "21.5em");
+
   }
   else if (window.matchMedia('(max-width: 992px)').matches) {
     $('#chatbot').css('height', "27em");
+    $("#chat-button").css("bottom", "26.5em");
+
   }
   else {
     $('#chatbot').css('height', "37em");
+    $("#chat-button").css("bottom", "36.5em");
+
   }
 }
